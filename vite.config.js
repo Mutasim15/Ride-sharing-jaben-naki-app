f20ab68da5ed8916d83aafc40de1b2fc9f1f296a
github_username <you@example.com>
2025-04-09 22:38:08 +0600

    update tailwind css

--- vite.config.js
+++ vite.config.js
@@ -12,13 +12,14 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     tailwindcss(),
     react()],
-  build: {
-    outDir: 'dist',
-  },
+    build: {
+      outDir: 'dist',
+    },
+  
 })
 
